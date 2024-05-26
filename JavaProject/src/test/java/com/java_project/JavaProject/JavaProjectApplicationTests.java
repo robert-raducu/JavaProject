08b36d07ccc22661827ca5b795f243ae112275c6
Robert <robert_raducu@yahoo.com>
2024-05-26 16:15:28 +0300

    project setup

---
+++ JavaProject/src/test/java/com/java_project/JavaProject/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_project.JavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
